8b56e508e37b0108421fda7a74f4b570c91f8563
jun <jchangh2@gmail.com>
2026-01-11 23:28:33 +0900

    renderer

--- src/3_orthographic_camera.ts
+++ src/3_orthographic_camera.ts
@@ -3,27 +3,24 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-const $canvas = document.getElementById("canvas") as HTMLCanvasElement;
-
-const CANVAS_WIDTH = 400;
-const CANVAS_HEIGHT = 200;
 
 const camera = new THREE.PerspectiveCamera(
   75,
-  CANVAS_WIDTH / CANVAS_HEIGHT,
+  window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 camera.position.z = 1.5;
-const renderer = new THREE.WebGLRenderer({ canvas: $canvas, antialias: true });
-renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
-// document.body.appendChild(renderer.domElement);
 
-// window.addEventListener("resize", () => {
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-//   camera.aspect = window.innerWidth / window.innerHeight;
-// });
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 new OrbitControls(camera, renderer.domElement);
 
